aa84f60638a420c5e264533da8d69641406190ee
Roger Wells <rsw359@gmail.com>
2022-03-02 04:53:06 +0900

    created render function

--- JS/app.js
+++ JS/app.js
@@ -5,12 +5,41 @@ let globalVar = allCards;
 // Term.prototype.render = function(){
 // console.log(this.term);
 
-for(let i = 0; i < allCards.length; i++){
+
+
+function getRandomIndex() {///Random number generator
+  return Math.floor(Math.random() * allCards.length)
+}
+
+
+let shownCards = [];//holds shown cards
+
+function renderCards() {
+  if (shownCards.length > 6){
+    delete shownCards[6];
+    delete shownCards[7];
+    delete shownCards[8];
+  }
+
+  while (shownCards.length < 6) {//as long as //set this to 6 numbers also
+    let randomnums = getRandomIndex();
+    while (!shownCards.includes(randomnums)) { //if the number is not present '!' 
+      shownCards.unshift(randomnums);
+    }
+  
+  }
+    
+  console.log(shownCards);
+
+  for(let i = 0; i < 3; i++){
     let newLi = document.createElement('li');
     let defLi = document.createElement('li');
-    newLi.textContent = allCards[i].term;
-    defLi.textContent = allCards[i].definition;
+    newLi.textContent = allCards[shownCards[i]].term;
+    defLi.textContent = allCards[shownCards[i]].definition;
     createLi.appendChild(newLi);
     createLi.appendChild(defLi);
+
   }
-// }
+}
+
+renderCards();
