9898f858608af3eacae5b945239284da508573d9
Bishal Khanal <bishal.khanal87@gmail.com>
2022-03-02 17:23:08 -0800

    updated terms and counter

--- JS/test.js
+++ JS/test.js
@@ -3,9 +3,10 @@
 console.log(allCards);
 
 // Array for questions for the test
-let testQuestions = [] //dont forget to clear this between tests
+let testQuestions = []; //dont forget to clear this between tests
+let score = 0;
 
-// let testForm = document.getElementById('test-form');
+let testForm = document.getElementById('test-form');
 let quizQ = document.getElementById('question');
 let answer1 = document.getElementById('aOne');
 let answer2 = document.getElementById('aTwo');
@@ -17,19 +18,64 @@ function getRandomIndex() {///Random number generator
 }
 
 //randomly selects Terms out of our constructor
-while(testQuestions.length < 5){
+while (testQuestions.length < 5) {
   let numIndex = getRandomIndex();
-  while(!testQuestions.includes(numIndex)){ //make sure we dont get duplicates
+  while (!testQuestions.includes(numIndex)) { //make sure we dont get duplicates
     testQuestions.push(numIndex);
   }
 }
 
 //start of the function that will generate new questions after clicking submit
 console.log(testQuestions);
-function handleClick(event){
-  quizQ.textContent = allCards[testQuestions[i]].question;
-  answer1.textContent = allCards[testQuestions[i]].definition;
-  answer2.textContent = allCards[testQuestions[i]].wrongAnswers[0];
-  answer3.textContent = allCards[testQuestions[i]].wrongAnswers[1];
-  answer4.textContent = allCards[testQuestions[i]].wrongAnswers[2];
+
+let j = 0;
+
+function renderQuestion() {
+  quizQ.textContent = allCards[testQuestions[j]].question;
+  answer1.textContent = allCards[testQuestions[j]].answers[0];
+  document.getElementById('answerOne').setAttribute('value', allCards[testQuestions[j]].answers[0]);
+  answer2.textContent = allCards[testQuestions[j]].answers[1];
+  document.getElementById('answerTwo').setAttribute('value', allCards[testQuestions[j]].answers[1]);
+  answer3.textContent = allCards[testQuestions[j]].answers[2];
+  document.getElementById('answerThree').setAttribute('value', allCards[testQuestions[j]].answers[2]);
+  answer4.textContent = allCards[testQuestions[j]].answers[3];
+  document.getElementById('answerFour').setAttribute('value', allCards[testQuestions[j]].answers[3]);
+  j++;
+
+
+
+  if (j === 5) {
+    testForm.removeEventListener('submit', handleSubmit);
+
+  }
+
+}
+
+
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+
+  let answerSelected = event.target.question1.value;
+  console.dir(answerSelected);
+  for (let i = 0; i < testQuestions.length; i++) {
+
+    if (answerSelected === allCards[testQuestions[i]].definition) {
+
+      score++;
+
+
+    }
+    
+  }
+  console.log(score);
+  renderQuestion();
+
+
+
+
 };
+renderQuestion();
+
+testForm.addEventListener('submit', handleSubmit);
